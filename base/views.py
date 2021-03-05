de5a8c38dd3859baa556ecf59bcbaa4a9a985509
nedaei79 <nedaei79@gmail.com>
2021-03-05 10:41:50 +0330

    contact us email

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.mail import send_mail
+from ssc.settings import EMAIL_HOST_USER
 
 
 def navbar(request):
@@ -13,10 +15,13 @@ def home(request):
 def contact_us(request):
     if request.method == "POST":
         inf = request.POST
+        title = str(inf.get('title'))
+        email = str(inf.get('email'))
         text = str(inf.get('text'))
         # print("*********************************")
         # print(len(text), text)
         if 10 <= len(text) <= 250:
+            send_mail(title, email + text, EMAIL_HOST_USER, ['webe21lopers@gmail.com'], fail_silently=False)
             return render(request=request, template_name='pages/successful.html', context={})
 
     return render(request=request, template_name='pages/contact_us.html', context={})
--- ssc/settings.py
+++ ssc/settings.py
@@ -120,3 +120,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# DataFlair
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'sarbaze.vaqean.gomnam@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'babakhalil'
+
+EMAIL_USE_TLS = True
+
+EMAIL_USE_SSL = False
